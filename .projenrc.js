4879efe79e2ed255df99c1dbbd236d0e744cbed1
Neil Kuan <guan840912@gmail.com>
2022-04-18 01:53:00 +0800

    chore: upgrade aws cdk version

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.3.0',
+  cdkVersion: '2.20.0',
   defaultReleaseBranch: 'main',
   name: 'eks-fargate-lab',
   depsUpgradeOptions: {
